4889e7b5ce18da4a9cf2720aa680bd23c98524d9
ybiquitous <10koba01@gmail.com>
2016-10-25 23:27:11 +0900

    Refactor webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,21 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const precss = require('precss')
 const autoprefixer = require('autoprefixer')
 
-const isProduction = process.env.NODE_ENV === 'production'
+const PRODUCTION = process.env.NODE_ENV === 'production'
 
-module.exports = {
+const config = {
   entry: {
     scripts: './src/scripts.js',
     styles: './src/styles.js',
   },
+
   output: {
     path: './public',
     filename: '[name].[hash].js',
   },
-  devtool: isProduction ? null : 'inline-source-map',
+
+  devtool: 'cheap-module-inline-source-map',
+
   module: {
     loaders: [
       {
@@ -28,19 +31,22 @@ module.exports = {
         test: /\.(s)?css$/,
         loader: ExtractTextPlugin.extract(
           'style',
-          `css?importLoaders=1&${isProduction ? 'minimize' : ''}`,
-          `postcss?${isProduction ? '' : 'sourceMap=inline'}`
+          `css?importLoaders=1${PRODUCTION ? '&minimize' : ''}`,
+          `postcss?${PRODUCTION ? '' : 'sourceMap=inline'}`
         ),
       },
     ],
   },
+
   resolve: {
     extensions: ['', '.js', '.jsx'],
   },
+
   postcss: () => [
     precss,
     autoprefixer,
   ],
+
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
@@ -49,11 +55,20 @@ module.exports = {
     }),
     new ExtractTextPlugin('[name].[hash].css'),
     new ManifestPlugin({ fileName: 'assets.json' }),
-  ].concat(isProduction ? [
+  ],
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = null
+
+  config.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
       },
-    }),
-  ] : []),
+    })
+  )
 }
+
+
+module.exports = config
