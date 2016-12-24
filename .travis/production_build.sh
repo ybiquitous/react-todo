cc018703ca0c57f45543113c95fd89c2846a8246
ybiquitous <10koba01@gmail.com>
2016-12-25 08:30:25 +0900

    Extract Bash script file

---
+++ .travis/production_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+NODE_ENV=production yarn build
+
+git add --force public/
+
+git commit \
+    --no-verify \
+    --author="Travis <10koba01@gmail.com>" \
+    --message="build files to deploy"
+
+git push --force origin/production
