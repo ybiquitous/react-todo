92d3003488bae654056d39e9c85f7ff1676cefbc
ybiquitous <10koba01@gmail.com>
2016-12-25 10:30:42 +0900

    Fix git command

--- .travis/production_build.sh
+++ .travis/production_build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+TARGET_BRANCH=production
+
+git branch -D $TARGET_BRANCH || true
+
+git checkout -b $TARGET_BRANCH remotes/origin/$TARGET_BRANCH
+
 NODE_ENV=production yarn build
 
 git add --force public/
@@ -10,4 +16,4 @@ git commit \
     --author="Travis <10koba01@gmail.com>" \
     --message="build files to deploy"
 
-git push --quiet --force "https://${GH_TOKEN}@github.com/ybiquitous/react-todo.git" production
+git push --quiet --force origin $TARGET_BRANCH
